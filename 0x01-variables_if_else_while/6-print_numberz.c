7b3b816e144cee7a94b086258f8f85177f0790ad
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-03-11 19:55:51 +0000

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - prints numbers 0-9 without using char or printf/puts,
- *
- *   * and putchar 2x max
- *
- *   (*
- *
- *    * Return: 0 on success
- *
- *     */
-
+ * main - prints numbers 0-9 without using char or printf/puts,
+ * and putchar 2x max
+(*
+ * Return: 0 on success
+ */
 int main(void)
-
 {
+	int i = '0';
 
-		int i = '0';
-
-
-
-			while (i <= '9')
-
-					{
-
-								putchar(i);
-
-										i++;
-
-											}
-
-				putchar('\n');
-
-					return (
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (
