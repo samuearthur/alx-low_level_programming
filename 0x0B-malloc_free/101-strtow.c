7ca289b4838a7ce2b78e2efa1cce4c7529894821
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-04-04 17:34:34 +0000

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,13 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
- */
+*  * strtow - concatenates all the arguments of your program
+*   *@str: string
+*    *@av: arguments
+*     * Return: a pointer to a new string
+*/
+
 char **strtow(char *str)
 {
 	int i, w, j, k, count, m, wordf;
