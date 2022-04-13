32c601a3c06e69895ec54d15dc67b92330c8b279
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-04-13 18:04:08 +0000

    task commit

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,9 @@
-#ifndef CALC_H
-#define CALC_H
-#include <stddef.h>
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct op - Struct op
  *
@@ -12,12 +15,12 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-/* Function for operating */
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-/* Pointer function */
+
 int (*get_op_func(char *s))(int, int);
-#endif /* CALC_H */
+#endif
