27e6658c31a9ddf1dee5d1050b8f8d8462fef8bc
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-04-13 17:59:07 +0000

    task commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,52 +1,78 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
 /**
- * op_add - entry point
- * @a: get number 1
- * @b: get number 2
- * Return: add between a and b
+ * op_add - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: add two input
+ * Return: value
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - entry point
- * @a: get number 1
- * @b: get number 2
- * Return: return subtation between a and b
+ * op_sub - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: subtract two input
+ * Return: value
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - entry point
- * @a: get number 1
- * @b: get number 2
- * Return: return multiplication between a and b
+ * op_mul - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: multiply two input
+ * Return: value
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - entry point
- * @a: get number 1
- * @b: get number 2
- * Return: return division between a and b
+ * op_div - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: divide two input
+ * Return: value
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 /**
- * op_mod - entry point
- * @a: get number 1
- * @b: get number 2
- * Return: return module between a and b
+ * op_mod - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: mod two input
+ * Return: value
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
