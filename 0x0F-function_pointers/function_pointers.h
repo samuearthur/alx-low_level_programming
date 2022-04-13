416c8a98bbf9574a5a91cf24c4c45ad1989421be
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-04-13 18:09:45 +0000

    task commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
-#include <stddef.h>
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* FUNCTION_POINTER_H */
+
+#endif
