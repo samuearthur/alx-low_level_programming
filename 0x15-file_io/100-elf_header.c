3019355102379ebf8420904520882dc3f43d1a79
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-04-29 11:24:02 +0000

    task commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,8 @@
+#include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <unistd.h>
 /**
  * _strncmp - compare two strings
  * @s1: the first string
